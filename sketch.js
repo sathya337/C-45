44935b323775370ed3b626b0bac1014980f52cff
sathya337 <57856433+sathya337@users.noreply.github.com>
2020-04-02 18:53:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+
+var playerInfo;
+var score = 0;
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(1800,1800);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
